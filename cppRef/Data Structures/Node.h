b2af331bff2daef8543acba6a2fa33a5e9599b56
georgejf <george.jing.fu@gmail.com>
2015-03-20 15:16:55 -0400

    cpp ref

---
+++ cppRef/Data Structures/Node.h
@@ -0,0 +1,12 @@
+template <typename T>
+
+class Node {
+public:
+    T data;
+    Node() {
+
+    };
+    Node(T d) {
+        data = d;
+    };
+};
---
+++ cppRef/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Data Structures/Node.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    Node<float> node;
+
+    cout << node.data;
+
+    // cin.get();
+    return 0;
+}
