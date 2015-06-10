bfda891da8fc86b596bbf540615644b7d9f1979b
George Fu <gjf@foundationcenter.org>
2015-06-10 13:59:40 -0400

    update comments

--- Internal/index.js
+++ Internal/index.js
@@ -2,6 +2,7 @@ var Namespace = {};
 Namespace.topics = {
     cat : 'main',
     'Software Engineering' : {
+        desc : 'The creation of software to avenge one\'s parents.',
         'Mathematics' : {
             'desc' : 'Rutgers \'09. With rigor, my friends.',
             '=' : 'Is not to be used for assignment or in place of the word \'is\' in a sentence.',
@@ -16,6 +17,7 @@ Namespace.topics = {
             }
         },
         'Algorithms' : {
+            desc : 'Traditional folk music from the land of Algoria, played quickly in the key of C major',
             'Time Complexity' : 'Equivalent to how much computing power needed when that is a constant.',
             'Space Complexity' : 'Memory storage needs.',
             'Recurrence Relation' : 'Defines a series of values with one equation.',
@@ -31,17 +33,23 @@ Namespace.topics = {
             'Kruskal\'s Tree' : 'MST via union find'
         },
         'Data and its Structures' : {
+            desc: '',
             'Memory' : 'RAM, that is. Holds your stuff! Becomes amnesic when you power off.',
-            'Pointer' : 'A memory address',
+            'Pointer' : 'A memory address held up by a lamppost',
             'Reference' : 'A constant pointer with automatic dereferencing, I think',
             'Object' : 'Tuna of the dirt. Ubiquitous, nutritious, and satisfying.',
-            'Linked List' : 'A thing and a pointer/reference to another thing',
+            'Linked List' : 'A thing and a pointer/reference to another thing -Jason',
+            'Unlinked List' : 'Well... Uh...',
             'Array' : {
+                'desc' : 'A battery of cannon placed in series.',
                 'C++' : 'Some kind of mythical engine for vectors that I\'ve never seen directly',
                 'PHP' : 'Swiss army hammer of collection types.',
                 'Python' : 'Is Pepsi okay?',
                 'C#' : 'An actual array, so you\'re probably looking for a list.',
                 'JavaScript' : 'Where arrays and functions can have secret string keys because they\'re all objects.'
+            },
+            'ArrayList' : {
+                'desc' : 'An array that resizes itself.'
             },
             'Hash Table' : {
                 'desc' : 'The basis of cache keying, and the world economy.',
@@ -51,15 +59,21 @@ Namespace.topics = {
                 'C#' : 'An actual hash table, so you\'re probably looking for a dictionary.',
                 'JavaScript' : 'Any and every object, including arrays & functions.'
             },
-            'Stack' : 'Last on, first off.',
+            'Stack' : 'Last on, first off. Also stack memory: faster access than the heap, exemplified by functional scope. You\'re in' +
+            'a function within a function, Leo!',
             'Queue' : '(British) First come first serve.',
+            'Graph' : 'A visual diagram describing the massive loss of revenue suffered due to your programming errors.',
             'Tree' : {
                 'desc' : 'A graph with a root and no circular paths.',
-                'Heap' : 'Ordered along any path.',
-                'Binary Search Tree' : 'Left is the opposite of right.'
+                'Heap' : 'Ordered along any path. Also heap memory: when you just dump your stuff in a pile but remember where' +
+                'everything is.',
+                'Binary Search Tree' : 'Left is the opposite of right.',
+                'Rotation': 'Dmitry Medvedev becomes President of Russia.'
             }
         },
         'Patterns & Principles' : {
+            'desc' : 'Lay bare your biases and make ready to defend them.',
+            'Snake Oil' : 'Design patterns',
             'Active Record' : 'It practically writes itself!',
             'Service Locator' : 'Inedible. I prefer the taste of DI.',
             'Data Mapper' : 'Responsible nemesis to the brash joy of ActiveRecord',
@@ -68,17 +82,25 @@ Namespace.topics = {
                 'Separation of Concerns' : 'The fire in the attic will not affect the temperature of the fire in the living room.',
                 'Single Responsibility Principle' : 'You had, one job.'
             },
+            'Functional' : {
+                'desc' : 'Square wagon wheels',
+                'Closure' : 'Entrapment of primitives and references of upper scope variables accessed by the function.',
+                Map : 'One size fits all. Lazy way to do a for loop.',
+                'Reduce/Fold' : '36-car pileup.',
+                Filter : 'A bouncer for collections.',
+                'Convolute/Zip' : 'Make things harder to understand.'
+            },
             'Liskov substitution' : 'Children should be able to replace their parents after 20 years of hard schooling.',
-            'Singleton' : 'Only one turtle.',
+            'Singleton' : 'The same turtle all the way down.',
             'Decorator' : 'Kinda like a wrapper. Modifies functions.',
             'Encapsulation' : 'You can\'t always get what you want.',
             '(web) Front Controller' : 'Run, application, run.',
-            'REST Service' : 'Why PUT when you can PATCH',
+            'REST Service' : 'Why PUT when you can PATCH?',
             'Asynchronicity' : 'Doing things synchronously, the hard way.',
             'Model View Controller' : 'And 3 million service factories',
             'Dependency Injection' : 'I need a thing that can do ...',
             'Test' : 'Who tests the testers?',
-            'Bitwise XOR' : '^_^ flipping bits for a living'
+            'Bitwise XOR' : '^_^ flipping burgers for a living'
         },
         'Object Orientation' : {
             'desc' : 'The organizing of concepts into classes whose names end with FactoryFactory',
@@ -97,7 +119,7 @@ Namespace.topics = {
             'GCC C++11' : 'Thank you for "auto"'
         },
         'PHP' : {
-            'desc' : 'It gets better!',
+            'desc' : 'It got better!',
             'Zend Framework 1': 'My first (2nd) MVC :D',
             'Zend Framework 2': 'Livin\' the easy life'
         },
@@ -106,22 +128,26 @@ Namespace.topics = {
             'MooTools': 'Kinda dead, but still great!',
             'jQuery' : 'Callback hell aside, JS made accessible to everyone',
             'AngularJS' : 'RAD SPA, really like the binding and model',
-            'Node.js' : 'Mysterious asynch issues in my WebStorm interpreter...'
+            'Node.js' : 'Build your JavaScript in your own home! Also the handy npm.',
+            'Backbone' : 'Erm... not too familiar. Not a fan of central event buses I sometimes see with backbone. '
         },
         'Python' : {
-            'desc' : 'List comprehensions and Multi-threa--- I mean processing',
+            'desc' : 'Generators, list comprehensions and Multi-threa--- I mean processing',
             'Tornado' : 'It\'s kinda like node.'
         },
         'C#' : {
+            desc: 'Cee Plus Plus Plus Plus',
             '.NET' : 'All said a really nice bundle of tools. Damn your method capitalization though.'
         },
         'Java' : {
+            desc : 'All the XML you can eat, and then some.',
             'Spring' : 'I know nothing about this.',
             'Maven' : 'I know nothing about this.',
             'Scala' : 'I know nothing about this.'
         },
         'Ruby' : {
-            'Rails' : 'Back in 2010 I could barely get this working in Ubuntu after 15 hours. Haven\'t touched it since.'
+            desc : '学んでいたときにの先生が日本人だったことが不思議じゃーーないよね。',
+            'Rails' : 'Back in 2010 I could barely get this working in Ubuntu after 15 hours. Haven\'t touched it much since.'
         },
         'SQL' : 'HEY JUDE, SELECT FROM JOIN, ON GROUP BY HAVING, ORDER LIMIT. ' +
             'DEALLOCATE, IF REPLICATE MODIFY, THEN PREPARE DISCONNECT COMMIT CONTINUE',
@@ -130,15 +156,16 @@ Namespace.topics = {
     'Software Tools' : {
         'Package Managers/VCS' : {
             'Git' : {
-                'desc' : 'rite of passage: being trapped in merge resolution and never seen again',
+                'desc' : 'rite of passage: being trapped in merge resolution and never seen again, detached head state',
                 'Git Flow' : 'good naming conventions hotfix/feature and observations about release cycles'
             },
-            'Subversion' : 'rite of passage: merge failing in strange ways',
+            'Subversion' : 'rite of passage: merge failing in strange ways, creating six thousand tree conflicts',
             'Mercurial': 'rite of passage: accidentally creating a new history',
             'Node Package Manager': 'install gulp',
             'Composer' : 'the autoloader generation is good'
         },
         'Test Automation' : {
+            desc: 'Who tests the testers?',
             'Protractor' : 'for AngularJS',
             'Jasmine' : 'to be and not to be',
             'TeamCity' : 'Better your CPU time than mine',
@@ -146,34 +173,42 @@ Namespace.topics = {
             'MSTest' : 'Yay'
         },
         'Frontend Build Automation' : {
+            desc: 'Everything\'s gonna be all right.',
             'Gulp' : 'I prefer LESS and not using strict JS to be honest',
             'Grunt' : 'Obsolesced by gulp in my workflow, unfortunately'
         },
         'Cloud' : 'namely AWS. Haven\'t really looked into Azure or AppEngine yet'
     },
     'Development Environment' : {
-        'JetBrains' : 'Heroes of our cause',
-        'Microsoft' : 'Not as bad as the rumors and jests of the open source world said',
-        'Ubuntu' : 'Easily download any software you want as long as you want the version from 6 years ago. Also a good lesson in bash.'
+        'Cygwin' : 'Linuxify your Windows a bit.',
+        'JetBrains' : 'Heroes of our cause. The recent CLion is also a great boon.',
+        'Microsoft' : 'Windows/VS/SQLSRV/IIS Now the good guys again.',
+        'Ubuntu' : 'Easily download any software you want as long as you want the version from 6 years ago. Also a good lesson in terminal.',
+        'Apple' : 'I can\'t understand or use effectively anything made by this company.'
     },
     'Project Management' : {
+        desc: 'Cat-herding has a proud tradition of futility',
         'Agile' : 'Animal Farm',
         'Team' : 'Mr. Baseball',
-        'Code Review' : 'I noticed you have a variable named Giraffe and a class named SeldonPlan',
+        'Code Review' : 'I noticed you have a variable named giraffe and a class named SeldonPlan',
         'Standardization' : 'My way or the highway, and for good reason'
+    },
+    'Other' : {
+        'Sports' : {
+            desc: 'I gave it all I could, coach, an 80% effort.',
+            'Table Tennis' : 'Classically trained.',
+            'Fencing' : 'Use your knees while you still can.',
+            'Baseball' : 'このシーズンで、終わってしまうかもしれない'
+        },
+        'Histories' : {
+            desc: 'The study of eagles. Dulce et decorum est...',
+            'Roman' : 'Eagles',
+            'Napoleonic' : 'Vive! l\'Em! p\'reur! More eagles',
+            'Second World War' : 'Evil eagles',
+            'Cold War': 'F-15 Strike Eagles',
+            'Modern Era': 'Door-kickers. No eagles anymore except America...'
+        }
     }
-    //'Other' : {
-    //    'Sports' : {
-    //        'Table Tennis' : '',
-    //        'Fencing' : '',
-    //        'Baseball' : ''
-    //    },
-    //    'Histories' : {
-    //        'Roman' : '',
-    //        'Napoleonic' : '',
-    //        'Second World War' : ''
-    //    }
-    //}
 };
 
 // Namespace.topics['Languages']['PHP']['Design Patterns'] = Namespace.topics['Computer Science']['Design Patterns'];
